34b736b4256c992a18c66607017cdb33894b7687
Fatma <fatmaulusal@gmail.com>
2019-12-27 15:16:53 -0500

    first commit

--- src/denemelerim/ReturnB11.java
+++ src/denemelerim/ReturnB11.java
@@ -1,12 +1,11 @@
 package denemelerim;
 
 //import java.util.Scanner;
-
 public class ReturnB11 {
     public static void main(String[] args) {
- //       boolean exists = false;
-   //     Scanner scan = new Scanner(System.in);
-   //     String word = scan.next();
+        //       boolean exists = false;
+        //     Scanner scan = new Scanner(System.in);
+        //     String word = scan.next();
         //Given a string word, print true if "java" appears starting at index 0 or 1 in the
         // string, such as with "javaxxx" or "xjavaxx" but not "xxjavaxx".
         // The string may be any length, including 0word = .
@@ -20,33 +19,47 @@ public class ReturnB11 {
         //input: c#javaruby
         //output: false
         //GET THE SECOND CRAHARECTER
-  String name ="Yasemin";
- char secondChar= name.charAt(1);
-        System.out.println(secondChar);
-        System.out.println( giveMe5());
-        int giveMethod= giveMe5();
-        System.out.println(giveMethod);
-     long firstYearSalary=mySalary();
-  //   long SecondYearSalary=firstYearSalary+firstYearSalary;
-        long secondYearSalary=(long)(firstYearSalary*0.5);
-        System.out.println(firstYearSalary+secondYearSalary);
-
-      //  System.out.println(secondYearSalary);
-        int age=0;
-        if(age>18){
-        }else{
-            System.out.println(isAdultOrNot(20));
-        }
+  //      String name = "Yasemin";
+   //     char secondChar = name.charAt(1);
+   //     System.out.println(secondChar);
+  //      System.out.println(giveMe5());
+  //      int giveMethod = giveMe5();
+   ///     System.out.println(giveMethod);
+   //     long firstYearSalary = mySalary();
+        //   long SecondYearSalary=firstYearSalary+firstYearSalary;
+     //   long secondYearSalary = (long) (firstYearSalary * 0.5);
+     //   System.out.println(firstYearSalary + secondYearSalary);
+        //  System.out.println(secondYearSalary);
+     //   boolean ageResult = (findTheAge(15));
+     //   System.out.println(ageResult);
+      //  System.out.println(findTheAge(26));
+        int huntingOfNumbers =MyNumbers(34,78);
+        System.out.println(huntingOfNumbers);
+        System.out.println(MyNumbers(45,6));
 
     }
-    public static int giveMe5(){
+
+
+    public static int giveMe5() {
 
         return 5;
     }
-    public static long mySalary(){
+
+    public static long mySalary() {
         return 1800000l;
     }
-    public static boolean isAdultOrNot(int age){
-        return true;
+
+    public static boolean findTheAge(int age) {
+//if(age>18){
+        //  return true;
+//}else{
+        //  return false;
+//}
+        //  }
+        return age > 18 && age < 28;
     }
-}
+  public static int MyNumbers(int num1,int num2){
+        int FindBigger=(num1>num2) ? num1: num2;
+        return FindBigger;
+  }
+    }
